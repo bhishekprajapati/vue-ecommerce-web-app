4d39255021113605f13aa6aaf8b815214ea4f237
Abhishek Prajapati <bhishekprajapati@gmail.com>
2022-07-15 23:27:18 +0530

    form layout added

---
+++ src/layouts/FormLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <form class="l-form th-card-border">
+    <header class="l-form__title">
+      <h3 class="h4-sb h2-sb:sm">
+        <slot name="form-title"> </slot>
+      </h3>
+    </header>
+    <ul>
+      <slot name="form-fields"></slot>
+    </ul>
+
+    <div class="l-form__btns">
+      <slot name="form-btns">
+        <button type="button" class="btn btn-primary btn-fluid">Primary</button>
+      </slot>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
