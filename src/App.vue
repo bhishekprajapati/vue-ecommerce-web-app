b589d2e159b212bc19f0e194d689ec6921199481
Abhishek Prajapati <bhishekprajapati@gmail.com>
2022-07-16 00:03:29 +0530

    form elements added

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,11 @@
 import TheHeader from './components/Navigation/TheHeader.vue';
 import TheFooter from './components/Navigation/TheFooter.vue';
 
-import TheHome from './views/TheHome.vue';
-import TheCart from './views/TheCart.vue';
-
 export default {
   name: 'App',
   components: {
     TheHeader,
     TheFooter,
-    TheHome,
-    TheCart
   }
 }
 </script>
---
+++ src/layouts/forms/FormFieldGroupLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="l-form__field-group l-form__field-group:sm">
+      <slot>
+          
+      </slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
