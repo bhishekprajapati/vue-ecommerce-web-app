acc42f30eaa2bff0d9fb424a50b48c67cbd635b4
Abhishek Prajapati <bhishekprajapati@gmail.com>
2022-07-28 17:47:09 +0530

    user account page added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,25 +2,36 @@ import TheHome from '../views/TheHome';
 import TheCart from '../views/TheCart';
 import TheCheckout from '../views/TheCheckout';
 import TheAuthentication from '../views/TheAuthentication';
+import TheUserAccount from "../views/TheUserAccount";
 
 export default [
     {
-        path: '/', 
+        path: '/',
+        name: 'home', 
         component: TheHome,
     },
 
     {
-        path: '/auth', 
+        path: '/account',
+        name: 'account',
+        component: TheUserAccount,
+    },
+
+    {
+        path: '/auth',
+        name: 'authentication', 
         component: TheAuthentication,
     },
 
     {
-        path: '/cart', 
+        path: '/cart',
+        name: 'cart', 
         component: TheCart,
     },
 
     {
-        path: '/checkout', 
+        path: '/checkout',
+        name: 'checkout',
         component: TheCheckout,
     },
 ];
---
+++ src/views/TheUserAccount.vue
@@ -0,0 +1,34 @@
+<template>
+    <GeneralSectionLayout>
+        <template v-slot:sub-title>
+            - welcome!
+        </template>
+        <template v-slot:title>
+            Account
+        </template>
+        <template v-slot:section-content>
+            <section class="th-card-bg" style="padding: 4%; border-radius: 1rem;">
+                <h3 class="h3-sb">Email</h3>
+                <p class="p2-r">{{ $store.getters.userEmail }}</p>
+
+                <button type="button" class="btn btn-secondary" style="margin-top: 1rem; border-color: red;">Logout</button>
+            </section>
+        </template>
+    </GeneralSectionLayout>
+</template>
+
+<script>
+import store from "../store/index";
+import GeneralSectionLayout from "../layouts/GeneralSectionLayout.vue";
+
+export default {
+    components: {
+        GeneralSectionLayout,
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log(to, from);
+        if(!store.getters.isUserAuthenticated) next({ name: 'authentication' });
+        else next();
+    }    
+}
+</script>
